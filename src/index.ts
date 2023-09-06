e1a105396c3bc735968254e77bf286128e463442
Sang Dang <sangdth@gmail.com>
2023-09-06 23:08:31 +0300

    small types improvement

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import type { ExtendedTheme } from './types';
+
 import minimalist from './minimalist';
 
-const themes: Record<string, unknown> = { minimalist };
+const themes: Record<string, ExtendedTheme> = { minimalist };
 
 export default themes;
